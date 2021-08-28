66f3b98d83dfa09c80c6824b17dfa71b225578a5
Shashi <shashikant.p@gmail.com>
2021-08-28 09:59:31 +0530

    Added documentation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const exrpess = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const exrpesshbs = require('express-handlebars');
 
+// Connect to Mongo DB
 mongoose.connect('mongodb://localhost:27017/rentals');
 
+// Define the Structure for the User collection
 const userSchema = mongoose.Schema({
     email: String,
     firstName : String,
@@ -11,9 +13,11 @@ const userSchema = mongoose.Schema({
     password: String
 });
 
+// Register the collection with Mongoose
 const User = mongoose.model("user", userSchema);
 
-const app = exrpess();
+const app = express();
+
 // Handlebars setup
 app.engine('handlebars',exrpesshbs());
 app.set('view engine', 'handlebars');
@@ -34,6 +38,7 @@ const items = [{
 
 const isAdmin = false;
 
+// This is the home screen
 app.get("/index", function(req, res) {
     res.render("hello", {
         userName: "Shashi",
@@ -43,16 +48,19 @@ app.get("/index", function(req, res) {
     });
 });
 
+// This is called to show login form
 app.get("/login", function(req, res) {
     res.render("login", {
         layout: 'main'
     });
 });
 
+// This is called to show registration page
 app.get("/register", function(req, res) {
     res.render("register", {layout: 'basic'});
 });
 
+// This is called when registration form is submitted
 app.get("/registerSubmit", async function(req, res) {
     const newUser = new User({
         email: req.query.email,
@@ -61,35 +69,42 @@ app.get("/registerSubmit", async function(req, res) {
         password: req.query.password,        
     });
 
+    // Insert new user document in mongo db
     await newUser.save();
 
+    // Send the user back to login page
     res.render("login", {layout: 'basic'});
 });
 
-
+// This is called when login form is submitted
 app.get("/loginSubmit", async function(req, res) {
     console.log(req.query.username);
     console.log(req.query.password);
 
+    // Find documents in mongodb matching the condition
     const user =  await User.findOne({
         email: req.query.username
     });
     
     console.log(user);
 
+    // If a document was found in the db continue
     if (user) {
+        // If the password is matching continue
         if (user.password === req.query.password) {
             res.render("hello", {
                 layout: 'basic',
                 userName: user.firstName
             });
         } else {
+            // If password is not matching, take to login page
             res.render("login", {
                 layout: 'basic',
                 message: "Please enter valid login details"
             });
         }
     } else {
+        // If no user found for the given email id, take back to login page
         res.render("login", {
             layout: 'basic',
             message: "Please enter valid login details"
@@ -99,4 +114,5 @@ app.get("/loginSubmit", async function(req, res) {
  
 });
 
+// Start the server and listen for requests
 app.listen(3002);
