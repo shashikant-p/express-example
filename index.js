b13f252cab68a03687b00e38a4dbc6d7d10d47a5
Shashi <shashikant.p@gmail.com>
2021-08-31 19:53:45 +0530

    Refactor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const exrpesshbs = require('express-handlebars');
-const dbModels = require('./db');
 const routes = require('./routes');
 
 const app = express();
@@ -10,22 +9,6 @@ const app = express();
 app.engine('handlebars',exrpesshbs());
 app.set('view engine', 'handlebars');
 app.use("/", routes);
-
-const items = [{
-    "name": "Item 1",
-    "description" : "test descrition 1",
-    "amouont" : "20.00"
-}, {
-    "name": "Item 2",
-    "description" : "test descrition 2",
-    "amouont" : "30.00"
-}, {
-    "name": "Item 3",
-    "description" : "test descrition 3",
-    "amouont" : "40.00"
-}];
-
-
 
 // Start the server and listen for requests
 app.listen(3002);
